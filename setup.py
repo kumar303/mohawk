a9b63f33b259d08153030cd0fb7906941eba8f23
Kumar McMillan <kumar.mcmillan@gmail.com>
2014-02-05 15:10:10 -0600

    setup description

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 
 setup(name='mohawk',
       version='0.0.1',
-      description="Work with Hawk authenticated requests/responses",
+      description="Library for Hawk HTTP authentication",
       long_description='',
-      author='Kumar McMillan',
+      author='Kumar McMillan, Austin King',
       author_email='kumar.mcmillan@gmail.com',
       license='MPL 2.0 (Mozilla Public License)',
       url='https://github.com/kumar303/mohawk',
